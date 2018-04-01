084d11dd7fc875bfc07e07e7d2e7a520adc52fdb
Daniel Langesten <daniel.langest@gmail.com>
2018-04-01 15:17:48 +0200

    Useless gaps

--- dwm-6.1.h
+++ dwm-6.1.h
@@ -17,6 +17,7 @@ static const char selbgcolor[]      = FG_COLOR;
 static const char selfgcolor[]      = BG_COLOR;
 
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
