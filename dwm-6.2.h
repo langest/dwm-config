8c074e5b19354f5afe022ee2b3255f54474846b0
Daniel Langesten <daniel.langest@gmail.com>
2020-07-04 10:47:09 +0100

    Change color theme to match gruvbox

--- dwm-6.2.h
+++ dwm-6.2.h
@@ -8,15 +8,27 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Ubuntu:size=10", "Kochi Mincho:size=10" };
 static const char dmenufont[]       = "Ubuntu:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char norm_bg[]      = "#ebdbb2";
+static const char norm_fg[]      = "#282828";
+static const char norm_border[]  = "#282828";
+
+static const char sel_bg[]       = "#689d6a";
+static const char sel_fg[]       = "#282828";
+static const char sel_border[]   = "#b16286";
+
+/* default colors
+static const char norm_bg[]      = "#222222";
+static const char norm_border[]  = "#444444";
+static const char norm_fg[]      = "#bbbbbb";
+static const char sel_fg[]       = "#eeeeee";
+static const char sel_bg[]       = "#005577";
+*/
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg       bg       border   */
+	[SchemeNorm] = { norm_fg, norm_bg, norm_border },
+	[SchemeSel]  = { sel_fg,  sel_bg,  sel_border  },
 };
 
 /* tagging */
@@ -60,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *firefoxcmd[]   = { "firefox", NULL };
 static const char *lockcmd[]      = { "sh", "/home/langest/dotfiles/scripts/lock.sh", NULL };
